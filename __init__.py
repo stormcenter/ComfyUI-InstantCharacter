3ca5778e5600f79e367167cccac216a2e6d3521f
多彩AI <18633108@qq.com>
2025-04-29 14:47:41 +0800

    add InstantCharacterGenerateWithStyleLora
    InstantCharacterGenerateWithStyleLora support

--- __init__.py
+++ __init__.py
@@ -2,17 +2,19 @@
 
 
 # 注册节点
-from .nodes.comfy_nodes import InstantCharacterLoadModel, InstantCharacterGenerate
+from .nodes.comfy_nodes import InstantCharacterLoadModel, InstantCharacterGenerate, InstantCharacterGenerateWithStyleLora
 
 
 NODE_CLASS_MAPPINGS = {
     "InstantCharacterLoadModel": InstantCharacterLoadModel,
     "InstantCharacterGenerate": InstantCharacterGenerate,
+    "InstantCharacterGenerateWithStyleLora": InstantCharacterGenerateWithStyleLora,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "InstantCharacterLoadModel": "InstantCharacter Load Model",
     "InstantCharacterGenerate": "InstantCharacter Generate",
+    "InstantCharacterGenerateWithStyleLora": "InstantCharacter Generate with Style Lora",
 } 
 WEB_DIRECTORY = "./web"
 
